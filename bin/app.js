3baa44fd4feceea8db0b3551267e38e34ae09d27
mzaini30 <muhzaini30@gmail.com>
2018-11-15 05:51:40 +0800

    oke

---
+++ bin/app.js
@@ -0,0 +1,22 @@
+var get_url_parameter = function get_url_parameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }   
+};
+
+if (get_url_parameter('url') != undefined){
+    location.href = get_url_parameter('url')
+}
+
+if (get_url_parameter('wa') != undefined){
+    location.href = 'whatsapp://send?text=' + get_url_parameter('wa')
+}
